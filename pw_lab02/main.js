b4bf786303c4d75b0ed0228f499793a78c31b7df
petewurster <pwurste1@student.ccp.edu>
2020-01-22 13:59:23 -0500

    remove recursion and add greedy regex

--- pw_lab02/main.js
+++ pw_lab02/main.js
@@ -1,18 +1,14 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded',() =>
-	document.getElementById('replaceButton').addEventListener('click', replaceText));
+document.addEventListener('DOMContentLoaded',() => {
+	document.getElementById('replaceButton').addEventListener('click', replaceText)
+});
 
 function replaceText() {
 	let env = grabState();
-	//fail-fast
-	if(env.find === '' || env.find === null) { return }
-
-	let regex = new RegExp(`${env.find}`, `${env.ignoreCase? 'i': ''}`);
+	let regex = new RegExp(`${env.find}`, `${env.ignoreCase? 'i': ''}${env.replaceAll? 'g': ''}`);
 
 	document.getElementById('textArea').value = env.textArea.replace(regex, env.replace);
-	//recursive as needed
-	if(env.textArea.match(regex) !== null && env.replaceAll === true) { replaceText() }
 }
 
 function grabState() {
