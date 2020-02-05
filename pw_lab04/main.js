4021a725d276b1740c91df7b819d19df2f432191
petewurster <pwurste1@student.ccp.edu>
2020-02-05 10:24:28 -0500

    better

--- pw_lab04/main.js
+++ pw_lab04/main.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', main);
 const API_ROOT_URL = 'https://messenger.ccp-lessons.com/message';
 
 const displayPost = (data) => {
-	document.getElementById('messageText').value = '';
 	document.getElementById('id').textContent = `post #${data.id}`;
 	document.getElementById('message').textContent = `${data.message}`;
 	document.getElementById('user').textContent = `--${data.jnum}`;
@@ -20,16 +19,17 @@ let xMit = {
 function postMessage() {
 	xMit.method = "POST";
 	xMit.body = JSON.stringify(getState());
+	document.getElementById('messageText').value = '';
 
 	fetch(API_ROOT_URL, xMit)
 		.then((reply) => reply.json())
-		.then((json) => pullMessage(json.id, displayPost));
+		.then((json) => pullMessage(getState().jnum, json.id, displayPost));
 }
 
-function pullMessage(messageId, callbackJustBecause) {
+function pullMessage(jnum, messageId, callbackJustBecause) {
 	xMit.method = "GET";
 	xMit.body = null;
-	let address = API_ROOT_URL + `/${getState().jnum}/${messageId}`;
+	let address = API_ROOT_URL + `/${jnum}/${messageId}`;
 
 	fetch(address, xMit)
 		.then((reply) => reply.json())
@@ -42,7 +42,6 @@ function getState() {
 		message: document.getElementById('messageText').value
 	}
 }
-
 
 function main() {
 	document.getElementById('post').addEventListener('click', postMessage);
